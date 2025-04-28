9546b84b5268c4f7b5a3922a3d626435f2486042
petesimple <petesimple@gmail.com>
2025-04-28 11:51:48 -0500

    Update app.js
    lap time counted

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let timer;
 let timeRemaining = 33 * 60 + 20; // seconds
 let lapCount = 0;
+let laps = []; // Store lap timestamps
 
 const timerDisplay = document.getElementById('timer');
 const startButton = document.getElementById('startButton');
@@ -22,8 +23,20 @@ function countdown() {
     updateTimer();
   } else {
     clearInterval(timer);
-    summaryDisplay.innerHTML = '<strong>Race Finished!</strong><br>You ran <strong>' + lapCount + '</strong> laps (~' + (lapCount * 0.4).toFixed(2) + ' km)';
+    summaryDisplay.innerHTML += '<br><strong>Race Finished!</strong>';
   }
+}
+
+function recordLap() {
+  const minutes = Math.floor(timeRemaining / 60);
+  const seconds = timeRemaining % 60;
+  const lapTime = minutes + ':' + seconds.toString().padStart(2, '0');
+  laps.push('Lap ' + lapCount + ' - ' + lapTime);
+  updateLapLog();
+}
+
+function updateLapLog() {
+  summaryDisplay.innerHTML = laps.join('<br>');
 }
 
 startButton.addEventListener('click', function() {
@@ -34,16 +47,23 @@ startButton.addEventListener('click', function() {
 addLapButton.addEventListener('click', function() {
   lapCount++;
   lapDisplay.textContent = lapCount;
+  recordLap();
 });
 
 subtractLapButton.addEventListener('click', function() {
-  if (lapCount > 0) lapCount--;
-  lapDisplay.textContent = lapCount;
+  if (lapCount > 0) {
+    lapCount--;
+    lapDisplay.textContent = lapCount;
+    laps.pop(); // Remove last recorded lap
+    updateLapLog();
+  }
 });
 
 resetLapButton.addEventListener('click', function() {
   lapCount = 0;
+  laps = [];
   lapDisplay.textContent = lapCount;
+  updateLapLog();
 });
 
 updateTimer();
